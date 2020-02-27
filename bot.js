a6b228763c885f8e432edb95fcd037f619d0d62e
Yurist-bek <60729565+Yurist-bek@users.noreply.github.com>
2020-02-27 05:57:55 +0500

    projectga yangi faq  papkasi qushildi va 2 ta faylga uzgarishkrirtildi !!!

--- bot.js
+++ bot.js
@@ -5,12 +5,13 @@ const session = require('telegraf/session');
 const Stage = require('telegraf/stage');
 const startScene = require('./scenes/start');
 const homeScene = require('./scenes/home');
+const faqSence = require('./scenes/FAQ')
 
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const stage = new Stage([startScene, homeScene], { default: 'START_SCENE' });
+const stage = new Stage([startScene, homeScene, faqSence ], { default: 'START_SCENE' });
 bot.use(session());
 bot.use(stage.middleware());
 
