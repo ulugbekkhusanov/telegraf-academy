8d1d67de5b8514f2d849f813c17a6140b7aa369d
KO-user <58812997+KO-user@users.noreply.github.com>
2020-02-27 10:57:02 +0500

    shamsqamar course page ni qushdi

--- bot.js
+++ bot.js
@@ -5,12 +5,13 @@ const session = require('telegraf/session');
 const Stage = require('telegraf/stage');
 const startScene = require('./scenes/start');
 const homeScene = require('./scenes/home');
+const courseScene = require('./scenes/course');
 
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const stage = new Stage([startScene, homeScene], { default: 'START_SCENE' });
+const stage = new Stage([startScene, homeScene, courseScene], { default: 'START_SCENE' });
 bot.use(session());
 bot.use(stage.middleware());
 
